37eb5af77d14406bcfc89a496547da0b0ed1c789
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2024-08-16 17:56:50 -0300

    added swaggerdocs

--- src/ForumCRUD.API/Program.cs
+++ src/ForumCRUD.API/Program.cs
@@ -1,6 +1,12 @@
 using Amazon;
 using ForumCRUD.API.Data;
+using ForumCRUD.API.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +22,41 @@ builder.Services.AddDbContext<ForumContext>(opts =>
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ForumAPI",
+        Version = "v1",
+        Description = "Projeto de API para um Fórum de Discussão",
+        Contact = new OpenApiContact
+        {
+            Name = "Vitor Carvalho",
+            Url = new System.Uri("https://github.com/VitorCCarvalho"),
+            Email = "vitorcoscarvalho@gmail.com"
+        }
+    });
+    
+});
+
+builder.Services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ForumContext>()
+                .AddDefaultTokenProviders();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("177.76.128.153",
+                                              "https://localhost:44476")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod(); ;
+                      });
+});
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
